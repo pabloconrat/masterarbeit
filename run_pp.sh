e2abc8a1b75becd76bdae21cce29ecc834da87c3
Pablo.Conrat <pablo.conrat@physik.uni-muenchen.de>
2022-03-21 09:25:54 +0100

    Module Load Removed from Run-Script
    Script runs as an interactive bash-script now instead of a login shell.

--- run_pp.sh
+++ run_pp.sh
@@ -1,13 +1,9 @@
-#!/bin/sh -l
-#SBATCH -J EMIL_pp              # Specify job name
-#SBATCH -p prepost             # Use partition prepost
-#SBATCH -N 1                   # Specify number of nodes
-#SBATCH -n 3                   # Specify max. number of tasks to be invoked
-#SBATCH --mem-per-cpu=5300     # Set memory required per allocated CPU
+#!/bin/bash 
+#SBATCH -J EMIL_pp             # Specify job name
+#SBATCH -p shared              # Use partition prepost
+#SBATCH --mem=20000
 #SBATCH -t 01:00:00            # Set a limit on the total run time
 #SBATCH -A bd1022              # Charge resources on this project account
-#SBATCH -o EMIL_pp_%j.out          # File name for standard and error output
-module load python3
+#SBATCH -o EMIL_pp_%j.out      # File name for standard and error output
 
-python3 /home/b/b381739/masterarbeit/postprocessing.py EMIL_SW06_tst4
-
+python3 /home/b/b381739/masterarbeit/postprocessing.py EMIL_SWl6_dy90
