14f7aeb5b2701d0a43257a7104a73fac572d02a4
Pablo.Conrat <pablo.conrat@physik.uni-muenchen.de>
2022-08-05 18:36:23 +0200

    Increased PP Memory Usage due to more Functionality

--- run_pp.sh
+++ run_pp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 #SBATCH -J EMIL_pp             # Specify job name
 #SBATCH -p shared              # Use partition prepost
-#SBATCH --mem=25000
+#SBATCH --mem=28000
 #SBATCH -t 01:00:00            # Set a limit on the total run time
 #SBATCH -A bd1022              # Charge resources on this project account
 #SBATCH -o EMIL_pp_%j.out      # File name for standard and error output
 
-python3 /home/b/b381739/masterarbeit/postprocessing.py EMIL_SW120f100 --years 2000 2001 --eofs
+python3 /home/b/b381739/masterarbeit/postprocessing.py EMIL_HSl0_dy60 --years 2000 --eofs 
 
